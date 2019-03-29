76bed14d451daccb30ffd8e4fac79fc16b27d2c4
Bekzod Abakulov <bek.abakulov@gmail.com>
2019-03-29 09:56:30 +0500

    Odnomerniy segment derevo done ;-)

--- segment-tree.js
+++ segment-tree.js
@@ -11,8 +11,50 @@ function dummySegmentTree(array, fn, N) {
 }
 
 function segmentTree(array, fn, N) {
-  return neutralTree(N);
-};
+  let tree = new Array(array.length * 4);
+  let v = 1;
+  let tl = 0;
+  let tr = array.length - 1;
+
+  function createTree(array, v, tl, tr) {
+    if (tl == tr) {
+      tree[v] = array[tl]
+    } else {
+      let tm = Math.floor((tl + tr) / 2);
+      createTree(array, v * 2, tl, tm);
+      createTree(array, v * 2 + 1, tm + 1, tr);
+      tree[v] = fn(tree[v * 2], tree[v * 2 + 1]);
+    }
+  }
+  if (array.length > 0) {
+    createTree(array, v, tl, tr);
+  }
+  return function(left, right) {
+    function query(v, tl, tr, l, r) {
+      if (l > r) {
+        return N;
+      }
+      if (l == tl && r == tr) {
+        return tree[v];
+      }
+      let tm = Math.floor((tl + tr) / 2);
+      let ql = query(v * 2, tl, tm, l, Math.min(r, tm));
+      let qr = query(v * 2 + 1, tm + 1, tr, Math.max(l,tm+1), r);
+      return fn(ql,qr);
+    }
+
+    if (left == right) {
+      return N;
+    } else if (left > right) {
+      throw new Error('Левый индекс должен быть меньше правого :)');
+    } else if (left >= array.length || right > array.length || left < 0) {
+      throw new Error('Элементы должны быть в границах массива :)');
+    } else {
+      return query(v, tl, tr, left, right - 1);
+    }
+  }
+  //return neutralTree(N);
+}
 
 function recursiveSegmentTree(array, fn, N) {
   return segmentTree(array, fn, N);
